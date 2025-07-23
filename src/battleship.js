4bfc70d5c143e831d88211901f6f2fbe5bd95fe7
Sekaii22 <shawn.ng22@gmail.com>
2025-07-23 17:57:20 +0800

    Add Gameboard class and the tests for it

--- src/battleship.js
+++ src/battleship.js
@@ -4,6 +4,9 @@ class Ship {
     sunk = false;
 
     constructor(length=1) {
+        if (length < 1)
+            length = 1;
+
         this.length = length;
     }
 
@@ -18,9 +21,105 @@ class Ship {
         if (this.hits === this.length) {
             this.sunk = true;
         }
-
-        return this.sunk;
     }
 }
 
-export { Ship };
+class Gameboard {
+    // board size of 10 rows, 10 cols
+    board = [
+                Array(10), 
+                Array(10), 
+                Array(10), 
+                Array(10), 
+                Array(10), 
+                Array(10), 
+                Array(10), 
+                Array(10), 
+                Array(10), 
+                Array(10)
+            ];
+    ships = [];
+    striked = [];           // coors where attacks received correctly striked ships
+    missed = [];            // coors where attacks missed
+    noOfShipsSunk = 0;
+
+    checkValidPlacement(coors) {
+        for (let [x, y] of coors) {
+            // check if coordinates outside of board
+            if (x < 0 || x > 9 || y < 0 || y > 9) {
+                return false;
+            }
+            // check if cell alr occupied
+            else if (this.board[x][y] != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    placeShip(length=1, [x, y], orientation="h") {
+        if (!["h", "v"].includes(orientation)) {
+            orientation = "h";
+        }
+
+        let coors = Array(length);
+        for (let i = 0; i < length; i++) {
+            if (orientation === "h") {
+                coors[i] = [x+i, y];
+            }
+            else {
+                coors[i] = [x, y+i];
+            }
+        }
+
+        if(!this.checkValidPlacement(coors)) {
+            return null;
+        };
+
+        const ship = new Ship(length);
+        for (let [x, y] of coors) {
+            this.board[x][y] = ship;
+        }
+        this.ships.push(ship);
+
+        return ship;
+    }
+
+    receiveAttack([x, y]) {
+        const ship = this.board[x][y];
+
+        if (ship != null) {
+            ship.hit();
+            
+            if (ship.sunk === true)
+                this.noOfShipsSunk += 1;
+            
+            if (this.noOfShipsSunk === this.ships.length)
+                this.#report();
+            
+            this.striked.push(String(x) + String(y));
+        }
+        else {
+            this.missed.push(String(x) + String(y));
+        }
+    }
+
+    #report() {
+        console.log("All friendly ships sunk!");
+    }
+}
+
+class Player {
+    gameboard = new Gameboard();
+}
+
+class RealPlayer extends Player {
+
+}
+
+class ComputerPlayer extends Player {
+
+}
+
+export { Ship, Gameboard, RealPlayer, ComputerPlayer };
--- test/battleship.test.js
+++ test/battleship.test.js
@@ -1,25 +1,100 @@
-import { Ship } from "../src/battleship";
+import { Ship, Gameboard, RealPlayer, ComputerPlayer } from "../src/battleship";
 
 describe("Ship", () => {
-    test("Takes a hit", () => {
+    test("hit()", () => {
         const ship = new Ship(2);
         ship.hit();
+
         expect(ship.hits).toBe(1);
     });
 
-    test("Sinking the ship when hits taken equals length", () => {
+    test("hit(): Sinking the ship when hits taken equals length", () => {
         const ship = new Ship(2);
         ship.hit();
         ship.hit();
+
         expect(ship.sunk).toBe(true);
     })
 
-    test("Stop incrementing hit when ship is already sunk", () => {
+    test("hit(): Stop incrementing hit when ship is already sunk", () => {
         const ship = new Ship(2);
         ship.hit();
         ship.hit();
         // sunked, try hitting again
         ship.hit();
+
         expect(ship.hits).toBe(2);
+    });
+});
+
+describe("Gameboard", () => {
+    test("placeShip(): Place ship of length 2 horizontally starting at (0,0) and ending at (0,1)", () => {
+        const gameboard = new Gameboard();
+        const ship = gameboard.placeShip(2, [0, 0]);
+
+        expect(gameboard.board[0][0]).toBe(ship);
+        expect(gameboard.board[1][0]).toBe(ship);
+    });
+
+    test("placeShip(): Place ship of length 2 vertically starting at (0,0) and ending at (1,0)", () => {
+        const gameboard = new Gameboard();
+        const ship = gameboard.placeShip(2, [0, 0], "v");
+
+        expect(gameboard.board[0][0]).toBe(ship);
+        expect(gameboard.board[0][1]).toBe(ship);
+    });
+
+    test("placeShip(): Placing a ship on cell already occupied by another ship should not be allowed", () => {
+        const gameboard = new Gameboard();
+        gameboard.placeShip(2, [0,0]);
+        const ship = gameboard.placeShip(2, [0, 0]);
+
+        expect(ship).toBeNull();
+    });
+
+    test("placeShip(): Placing a ship outside board should not be allowed", () => {
+        const gameboard = new Gameboard();
+        const ship = gameboard.placeShip(2, [9, 0]);
+
+        expect(ship).toBeNull();
+    });
+
+    test("receiveAttack()", () => {
+        const gameboard = new Gameboard();
+        const ship = gameboard.placeShip(2, [0, 0]);
+        gameboard.receiveAttack([0, 0]);
+
+        expect(ship.hits).toBe(1);
+        expect(gameboard.striked).toEqual(["00"]);
+    });
+    
+    test("receiveAttack(): sink a ship", () => {
+        const gameboard = new Gameboard();
+        const ship = gameboard.placeShip(2, [0, 0]);
+        gameboard.receiveAttack([0, 0]);
+        gameboard.receiveAttack([1, 0]);
+        
+        expect(ship.hits).toBe(2);
+        expect(ship.sunk).toBe(true);
+        expect(gameboard.noOfShipsSunk).toBe(1);
+    });
+
+    test("receiveAttack(): missed", () => {
+        const gameboard = new Gameboard();
+        const ship = gameboard.placeShip(2, [0, 0]);
+        gameboard.receiveAttack([9, 9]);
+        
+        expect(ship.hits).toBe(0);
+        expect(gameboard.missed).toEqual(["99"]);
+    });
+});
+
+describe("Player", () => {
+    test("RealPlayer and ComputerPlayer contains a gameboard object", () => {
+        const realPlayer = new RealPlayer();
+        const computerPlayer = new ComputerPlayer();
+        
+        expect(realPlayer.gameboard).not.toBeNull();
+        expect(computerPlayer.gameboard).not.toBeNull();
     });
 });
