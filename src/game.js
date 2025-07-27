8b057d3c041b484cee2faa7952caa209106ea082
Sekaii22 <shawn.ng22@gmail.com>
2025-07-27 15:39:21 +0800

    Slight update to game.js

--- src/game.js
+++ src/game.js
@@ -1,14 +1,15 @@
-import { Ship, Gameboard, Player } from "./battleship";
+import { Player } from "./player";
 
 class Game {
     player1;
     player2;
+    winner;
     turn = 0;
     gameOver = false;
 
-    constructor(p1Name, p2Name, p1ShipDetails=[], p2ShipDetails=[]) {
+    constructor(p1Name, p2Name, p1ShipDetails=[], p2ShipDetails=[], ai=true) {
         this.player1 = new Player(p1Name);
-        this.player2 = new Player(p2Name);
+        this.player2 = new Player(p2Name, ai);
 
         for (let detail of p1ShipDetails) {
             this.player1.gameboard.placeShip(detail.length, detail.coor, detail.orientation, detail.name);
@@ -21,38 +22,50 @@ class Game {
 
     takeTurn(coor) {
         if (this.gameOver)
-            return false;
+            return null;
 
         let result;
-        if (this.turn % 2 === 0) {
-            // player1's turn to attack
+        if (this.whoseTurnIsIt() === 1) {
+            console.log(`P1 attacks ${coor}`);
             result = this.player2.gameboard.receiveAttack(coor);
         } 
         else {
-            // player2's turn to attack
+            console.log(`P2 attacks ${coor}`);
             result = this.player1.gameboard.receiveAttack(coor);
         }
         
         // invalid attack
-        if (result === null) {
-            return false;
-        }
+        if (result === null)
+            return null;
 
         this.checkGameOver();
         this.turn += 1;
 
-        return true;
+        return result;
+    }
+
+    whoseTurnIsIt() {
+        if (this.turn % 2 === 0) {
+            // player1's turn
+            return 1;
+        } 
+        else {
+            // player2's turn
+            return 2;
+        }
     }
 
     checkGameOver() {
         if (this.player1.gameboard.allShipsDestroyed) {
             console.log("Winner is " + this.player2.name);
             this.gameOver = true;
+            this.winner = this.player2;
         }
 
         else if (this.player2.gameboard.allShipsDestroyed) {
             console.log("Winner is " + this.player1.name);
             this.gameOver = true;
+            this.winner = this.player1;
         }
 
         return this.gameOver;
